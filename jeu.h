b68ad4defa7652bff1430c05834f3973eea8a5f3
Adam <adam.fatah@edu.ece.fr>
2025-12-20 14:05:42 +0100

    Correction finale jeu.h

--- jeu.h
+++ jeu.h
@@ -11,31 +11,33 @@ void initialiserNiveau(Niveau *niveau, int numNiveau);
 // Echange le contenu de deux cases (utilise des pointeurs de Case)
 void echangerCases(Case *case1, Case *case2);
 
-// --- FONCTIONS BONUS (ITEMS DE TON COLLEGUE) ---
-// Adaptées pour utiliser la structure 'Niveau'
+// --- EFFETS SPÉCIAUX (Figures du CDC) ---
 
-// Détruit une zone de 3x3 autour de la case (x,y)
-void itemBombe(Niveau *niveau, int x, int y);
+// Détruit une zone de 3x3 autour de la case (x,y) [EXTENSION niveau 2+]
+void effetBombe(Niveau *niveau, int x, int y);
 
-// Détruit toute la ligne indiquée
-void itemLigne(Niveau *niveau, int ligne);
+// Détruit toute la ligne indiquée (Croix de 9)
+void effetLigne(Niveau *niveau, int ligne);
 
-// Détruit toute la colonne indiquée
-void itemColonne(Niveau *niveau, int colonne);
+// Détruit toute la colonne indiquée (Croix de 9)
+void effetColonne(Niveau *niveau, int colonne);
 
-// Détruit toutes les cases d'une certaine couleur/type sur tout le plateau
-void itemCouleur(Niveau *niveau, int type);
+// Détruit toutes les cases d'une certaine couleur/type (6 alignés)
+void effetCouleur(Niveau *niveau, int type);
+
+// Détruit un carré 4x4 (Carré 4x4)
+void effetCarre4x4(Niveau *niveau, int x, int y);
 
 // --- LOGIQUE MATCH-3 ---
 
-// 1. Détecte les alignements (3 ou +), supprime les bonbons et met à jour le score/contrat
-// Retourne 1 si des bonbons ont été détruits, 0 sinon
+// Détecte les alignements (3 ou +), supprime les fruits et met à jour le score/contrat
+// Retourne 1 si des fruits ont été détruits, 0 sinon
 int detecterEtSupprimerAlignements(Niveau *niveau);
 
-// 2. Fait tomber les bonbons du haut vers le bas (Gravité) et remplit les trous
+// Fait tomber les fruits du haut vers le bas (Gravité) et remplit les trous
 void faireTomberEtRemplir(Niveau *niveau);
 
-// 3. Fonction Mère : Enchaine détection et gravité tant qu'il y a des combos
+// Fonction Mère : Enchaine détection et gravité tant qu'il y a des combos
 void gererCombos(Niveau *niveau);
 
 // --- VERIFICATION VICTOIRE ---
